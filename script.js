4be67fd550f9990d2bc5f44351c24d96e32c6982
dz4kiya <dishmatul@gmail.com>
2023-08-24 09:52:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// script.js
+const video = document.getElementById('heroVideo');
+const content = document.querySelector('.content');
+const scrollHeight = content.scrollHeight;
+
+// window.addEventListener('scroll', () => {
+//     const scrollPosition = window.scrollY;
+//     const videoTime = (scrollPosition / scrollHeight) * video.duration;
+//     video.currentTime = videoTime;
+// });
+
+
+// script.js
+function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => (inThrottle = false), limit);
+        }
+    };
+}
+
+window.addEventListener('scroll', throttle(() => {
+    const scrollPosition = window.scrollY;
+    const currentFrame = Math.floor((scrollPosition / scrollHeight) * totalFrames);
+    const videoTime = currentFrame / video.frameRate;
+    video.currentTime = videoTime;
+}, 50)); // Adjust the limit as needed
