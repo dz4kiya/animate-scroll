9c1a334a7df213ca5d3c3c3fdd6d32706299ba57
dz4kiya <dishmatul@gmail.com>
2023-08-31 15:10:31 +0700

    new pages

--- script.js
+++ script.js
@@ -10,23 +10,4 @@ window.addEventListener('scroll', () => {
 });
 
 
-// script.js
-// function throttle(func, limit) {
-//     let inThrottle;
-//     return function() {
-//         const args = arguments;
-//         const context = this;
-//         if (!inThrottle) {
-//             func.apply(context, args);
-//             inThrottle = true;
-//             setTimeout(() => (inThrottle = false), limit);
-//         }
-//     };
-// }
 
-// window.addEventListener('scroll', throttle(() => {
-//     const scrollPosition = window.scrollY;
-//     const currentFrame = Math.floor((scrollPosition / scrollHeight) * totalFrames);
-//     const videoTime = currentFrame / video.frameRate;
-//     video.currentTime = videoTime;
-// }, 50)); // Adjust the limit as needed
